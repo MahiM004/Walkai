f26d2d1e1991f65f3d3e896e067c80510a47e08b
Mahesh iOS <mahesh.m@mca140.com>
2016-02-25 11:06:07 +0530

    Initial Commit

---
+++ Walkai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Walkai
+//
+//  Created by Mahesh_iOS on 25/02/16.
+//  Copyright © 2016 MCA140. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Walkai/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Walkai
+//
+//  Created by Mahesh_iOS on 25/02/16.
+//  Copyright © 2016 MCA140. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
